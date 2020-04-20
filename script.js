c03a2d3a47ca729c9690e99eb7290d6a59f37f08
Michael Cervantes <mac95860@hotmail.com>
2020-04-19 22:04:45 -0500

    Mostly Done

--- script.js
+++ script.js
@@ -1,2 +1,113 @@
-var userInput = $("input");
+
+
+//---------------------------------------display all weather info and create city list item --------------------------------------
+
+function displayDate() {
+
+    var date = moment().format('MMMM Do YYYY');
+    var aCity = $("#citySearched").val();
+    $("h2").text(aCity + " " + date);
+
+}
+
+$("button").on("click", function (event) {
+    event.preventDefault();
+
+    var input = $("#citySearched").val();
+
+    var city = $("<li>");
+    city.addClass("list-group-item");
+    city.attr("data-name", input);
+
+    city.text(input);
+    city.val(input);
+    $("ul").append(city);
+
+    displayWeatherInfo();
+    displayDate();
+})
+
+//--------------------------------------display all weather info-------------------------------------------------------------------
+function displayWeatherInfo() {
+
+    $(".cardHolder").empty();
+
+    var input = $("#citySearched").val();
+    
+    var APIKey = "0246ddd8e1c43f29cf72682d14088ce6";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey + "&units=imperial";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var lon = response.city.coord.lon;
+        var lat = response.city.coord.lat;
+
+        queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=0246ddd8e1c43f29cf72682d14088ce6&units=imperial";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response);
+
+            mainTemp = response.current.temp;
+            todayTemp = $(".temp").text(mainTemp);
+            windSpeed = $(".wind").text(response.current.wind_speed);
+            var indexUV = $(".uvIndex").text(response.current.uvi);
+
+            for (var i = 0; i < 5; i++) {
+                var temp = response.list[i].main.temp;
+
+                var iconCode = response.daily[i].weather.icon;
+                var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                var weatherIcon = $("#icon").attr('src', iconUrl);
+
+                var newDiv = $("<div>").addClass("weatherCard");
+
+                var weekDay = response.daily[i].dt;
+                var weekDayDiv = $("<h5>").text(weekDay).appendTo(newDiv);
+
+                var iconDiv = $("<img>").attr('src', iconUrl).appendTo(newDiv);
+                var tempDiv = $("<p>").text(temp).appendTo(newDiv);
+
+                var weather = newDiv.appendTo(".cardHolder");
+            }
+        })
+    });
+}
+
+//------------------------------------------------display weather info for each city clicked on from list------------------------------
+$(document).on("click", "data-name", displayWeatherInfo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
