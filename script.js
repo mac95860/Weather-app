dff1900b82e533a8359dac91779609ccf9748799
Michael Cervantes <mac95860@hotmail.com>
2020-06-16 16:27:34 -0500

    List click works

--- script.js
+++ script.js
@@ -1,113 +1,100 @@
+//-------------On click function----------------------
+$("button").on("click", function (event) {
+    event.preventDefault(event)
+    var input = $("#citySearched").val();
+    addToList(input);
+    displayWeatherInfo(input);
+    displayDate(input);
+});
 
-
-//---------------------------------------display all weather info and create city list item --------------------------------------
-
-function displayDate() {
-
+//-------------function displaying the date------------------------
+function displayDate(input) {
     var date = moment().format('MMMM Do YYYY');
-    var aCity = $("#citySearched").val();
-    $("h2").text(aCity + " " + date);
+    
+    $("h2").text(`${input} ${date}`);
+};
 
+function addToList(input) {
+    var city = $("<li>")
+    .text(input)
+    .addClass("list-group-item")
+    .attr("data-name", input);
+
+    $(".list-group").append(city);
 }
 
+//-------------add city to list, call ajax to get------------------
+function displayWeatherInfo(input) {
 
+    $(".cardHolder").empty();
 
-//--------------------------------------display all weather info-------------------------------------------------------------------
-function displayWeatherInfo() {
+    var APIKey = "da856e33c208e368aea27f2610799369";
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${APIKey}units=imperial`;
 
-    $("button").on("click", function (event) {
-        event.preventDefault();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
-        $(".cardHolder").empty();
+        var mainTemp = response.current.temp;
+        var todayTemp = $(".temp").text(mainTemp);
+        var windSpeed = $(".wind").text(response.current.wind_speed);
 
-        var input = $("#citySearched").val();
-        var city = $("<li>")
-                    .text(input)
-                    .addClass("list-group-item")
-                    .attr("data-name", input);
-        
-        $("ul").append(city);
-        
-        
-        var APIKey = "0246ddd8e1c43f29cf72682d14088ce6";
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey + "&units=imperial";
+        uvIndex(response.city.coord.lon, response.city.coord.lat);
+        fiveDayForecatst(input);
+    });
+};
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            var lon = response.city.coord.lon;
-            var lat = response.city.coord.lat;
+//--------------------------get five day forecast---------------------------------------------------
 
-            queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=0246ddd8e1c43f29cf72682d14088ce6&units=imperial";
+function fiveDayForecatst(input) {
+    var APIKey = "da856e33c208e368aea27f2610799369";
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${APIKey}units=imperial`;
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 5; i < response.list.length; i += 6) {
+            //---------------creates a card that will hold daily forecast info------------------
+            var newDiv = $("<div>").addClass("weatherCard").appendTo(".cardHolder");
 
-                console.log(response);
+            //---------------creates a div to hold the weather image then retrieves the image--------
+            var iconDiv = $("<img>").attr('src', iconUrl).appendTo(newDiv);
+            var iconCode = response.daily[i].weather.icon;
+            var iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
-                mainTemp = response.current.temp;
-                todayTemp = $(".temp").text(mainTemp);
-                windSpeed = $(".wind").text(response.current.wind_speed);
-                var indexUV = $(".uvIndex").text(response.current.uvi);
+            //---------------creates a heading to hold the day of the week then retrieves date from API------------
+            var weekDayDiv = $("<h5>").text(weekDay).appendTo(newDiv);
+            var weekDay = response.daily[i].dt;
 
-                for (var i = 0; i < 5; i++) {
-                    var temp = response.list[i].main.temp;
+            //---------------creates a <p> that holds the temp then retrieves temp---------------------    
+            var tempDiv = $("<p>").text(temp).appendTo(newDiv);
+            var temp = `${response.list[i].main.temp}&deg;F`;
+        };
+    });
+};
 
-                    var iconCode = response.daily[i].weather.icon;
-                    var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-                    var weatherIcon = $("#icon").attr('src', iconUrl);
+//----------------------------------get uv index------------------------------------------
+function uvIndex(lon, lat) {
 
-                    var newDiv = $("<div>").addClass("weatherCard");
+    queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=0246ddd8e1c43f29cf72682d14088ce6&units=imperial`;
 
-                    var weekDay = response.daily[i].dt;
-                    var weekDayDiv = $("<h5>").text(weekDay).appendTo(newDiv);
-
-                    var iconDiv = $("<img>").attr('src', iconUrl).appendTo(newDiv);
-                    var tempDiv = $("<p>").text(temp).appendTo(newDiv);
-
-                    var weather = newDiv.appendTo(".cardHolder");
-                }
-            })
-        });
-
-    })
-
-    displayDate();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var uv = $(".uvIndex").val(response.city.coord.value);
+    });
 }
-
-displayWeatherInfo()
 
 //------------------------------------------------display weather info for each city clicked on from list------------------------------
-$(document).on("click", "data-name", displayWeatherInfo);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".list-group").on("click", "li", function() {
+    displayWeatherInfo($(this).text());
+    displayDate($(this).text());
+});
 
 
 
